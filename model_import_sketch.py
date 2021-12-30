757080ff218427734264632afb3df86efe631f7a
John Doe <johndoe@example.com>
2021-12-29 19:28:29 -0500

    convert to .py

--- model_import_sketch.py
+++ model_import_sketch.py
@@ -1,7 +1,6 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
 #     text_representation:
 #       extension: .py
 #       format_name: light
@@ -15,8 +14,13 @@
 # + [markdown] id="view-in-github" colab_type="text"
 # <a href="https://colab.research.google.com/github/xloem/techsketball/blob/main/model_import_sketch.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
 
-# + colab={"base_uri": "https://localhost:8080/"} id="nH1Ld_vd9wyx" outputId="a1c96dd3-4cd0-473f-a8f3-2fbebf303cc7"
-[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/googlecolab/colabtools/blob/master/notebooks/colab-github-demo.ipynb)
+# + colab={"base_uri": "https://localhost:8080/"} id="nH1Ld_vd9wyx" outputId="9d084e9b-c8b2-493c-ea3e-f2a288d43c44"
+#[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/googlecolab/colabtools/blob/master/notebooks/colab-github-demo.ipynb)
+
+starting_model_path = 't5-base'#'bigscience/T0pp'
+input_width = 512
+
+
 # #!pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 # !pip3 install deepspeed
 # !pip3 install transformers
@@ -24,29 +28,32 @@
 # !pip3 install sentencepiece
 # !git clone https://github.com/xloem/techsketball && ln -s techsketball/* .
 
-# + colab={"base_uri": "https://localhost:8080/"} id="mTGBjrXoX2eS" outputId="ce5bf3c5-0f0f-42bf-e43f-909f2b8c7917"
-import jax.tools.colab_tpu
-jax.tools.colab_tpu.setup_tpu()
-jax.local_devices()
+# + id="mTGBjrXoX2eS"
+#import jax.tools.colab_tpu
+#jax.tools.colab_tpu.setup_tpu()
+#jax.local_devices()
 
-# + id="qhJhFJfQAOXG"
 from transformers import T5Tokenizer, FlaxT5ForConditionalGeneration
-starting_model_path = 't5-base'#'bigscience/T0pp'
 
 tokenizer = T5Tokenizer.from_pretrained(starting_model_path) # only for source, not for binary
 model = FlaxT5ForConditionalGeneration.from_pretrained(starting_model_path)
 
-# + id="QnyDTDt_f1fE"
+# + colab={"base_uri": "https://localhost:8080/"} id="QnyDTDt_f1fE" outputId="36a849f6-4bb4-45f1-e8ef-dd8edf142fa5"
 from find_pycode import pair_finder
-data_tuples = [*pair_finder(globals())] # tuples of (bytes, str)
+print('generating training data ...')
+data_tuples = [
+  (bytes, str)
+  for bytes, str
+  in pair_finder(globals()) # finds code examples
+  if len(bytes) <= input_width
+]
 
-# + id="TrRadfeAhZSd" outputId="21397f28-7958-464e-f99e-d89b9e861cd1" colab={"base_uri": "https://localhost:8080/", "height": 397}
-input_width = max((len(bytes) for bytes, str in data_tuples))
-print('converting byteslist to jax array .. is there a faster way to do this?')
-data_input_ids = jax.numpy.array([
-    jax.numpy.pad(jax.numpy.array([*bytes], dtype=jax.numpy.uint8), input_width) for bytes, str in data_tuples
+# + id="TrRadfeAhZSd"
+import jax, numpy as np
+data_input_ids = jax.numpy.stack([
+  np.frombuffer(bytes.ljust(input_width, b'\0'), dtype=np.uint8)
+  for bytes, str in data_tuples
 ])
-print('converted')
 data_labels = [str for bytes, str in data_tuples]
 
 
