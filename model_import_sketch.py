eaaebe0cb0a267db513696233c778a020352daa2
xloem <0xloem@gmail.com>
2021-12-31 07:34:17 -0500

    -

--- model_import_sketch.py
+++ model_import_sketch.py
@@ -107,6 +107,50 @@ def data_loader(rng, dataset, batch_size, shuffle=True):
 
         yield batch
 
+# from run_t5_mlm_flax.py
+#rng = jax.random.PRNGKey(training_seed)
+#dropout_rngs = jax.random.split(rng, jax.local_device_count())
+#
+## Define gradient update step fn
+#def train_step(state, batch, dropout_rng):
+#    dropout_rng, new_dropout_rng = jax.random.split(dropout_rng)
+#                                                                                     def loss_fn(params):
+#        labels = batch.pop("labels")
+#
+#        logits = state.apply_fn(**batch, params=params, dropout_rng=dropout_r
+#ngain=True)[0]
+#                                                                                         # compute loss
+#        loss = optax.softmax_cross_entropy(logits, onehot(labels, logits.shape[-1])).mean()
+#
+#        return loss
+#    grad_fn = jax.value_and_grad(loss_fn)
+#    loss, grad = grad_fn(state.params)
+#    grad = jax.lax.pmean(grad, "batch")
+#    new_state = state.apply_gradients(grads=grad)
+#    metrics = jax.lax.pmean(
+#        {"loss": loss, "learning_rate": linear_decay_lr_schedule_fn(state.ste
+#p)xis_name="batch"
+#    )
+#    return new_state, metrics, new_dropout_rng
+#
+## Create parallel version of the train step
+#p_train_step = jax.pmap(train_step, "batch", donate_argnums=(0,))
+## Replicate the train state on each device
+#state = jax_utils.replicate(state)
+#
+#train_time = 0
+#epochs = tqdm(range(num_epochs), desc="Epoch ... ", position=0)
+#for epoch in epochs:                                                                 # ======================== Training ================================             train_start = time.time()                                                        train_metrics = []                                                                                                                                                # Create sampling rng                                                            rng, input_rng = jax.random.split(rng)
+#                                                                                         # Generate an epoch by shuffling sampling indices from the train dataset
+#        num_train_samples = len(tokenized_datasets["train"])
+#        train_samples_idx = jax.random.permutation(input_rng, jnp.arange(num_train_samples))                                                                              train_batch_idx = generate_batch_splits(train_samples_idx, train_batch_size)
+#        # Gather the indexes for creating the batch and do a training step               for step, batch_idx in enumerate(tqdm(train_batch_idx, desc="Training...", position=1)):
+#            samples = [tokenized_datasets["train"][int(idx)] for idx in batch_idx
+#]                                                                                            model_inputs = data_collator(samples)                                                                                                                             # Model forward
+#            model_inputs = shard(model_inputs.data)                                          state, train_metric, dropout_rngs = p_train_step(state, model_inputs, dropout_rngs)                                                                               train_metrics.append(train_metric)
+#                                                                                             cur_step = epoch * (num_train_samples // train_batch_size) + step
+#                                                                                             if cur_step % training_args.logging_steps == 0 and cur_step > 0:                     # Save metrics                                                                   train_metric = jax_utils.unreplicate(train_metric)                               train_time += time.time() - train_start                                          if has_tensorboard and jax.process_index() == 0:                                     write_train_metric(summary_writer, train_metrics, train_time, cur_step)                                                                                                                                                                        epochs.write(                                                                        f"Step... ({cur_step} | Loss: {train_metric['loss'].mean()}, Learning Rate: {train_metric['learning_rate'].mean()})"                                          )                                                                                                                                                                 train_metrics = []
+#
 
 # + id="aGdYRKVJxHxN"
 while True:
