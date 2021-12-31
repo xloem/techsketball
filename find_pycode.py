56c5ebee73a293546d5c825455b6735b4f02d6e0
John Doe <johndoe@example.com>
2021-12-31 13:24:34 -0500

    untested; harddisk issue

--- find_pycode.py
+++ find_pycode.py
@@ -42,6 +42,50 @@ class pair_finder:
                     assert not 'hist, item not recognised type'
             except ReferenceError:
                 continue
+
+def write_files(pfx, tokenizerpfx, input_width, tokenizer, label_width, *initial_objects, verbose = True):
+    import numpy as np
+    with open('{pfx}bin.u8.{input_width}vec', 'wb') as itok, open('{pfx}bin.attnmask.u8.{input_width}vec', 'wb') as iattn, open(f'{pfx}{tokenizerpfx}src.u16.{label_width}vec', 'wb') as otok, open(f'{pfx}{tokenizerpfx}.src.attnmask.u8.{label_width}vec', 'wb') as oattn:
+        iattn_buf = np.zeros(input_width, dtype=np.uint8)
+        for idx, (bin, src) in enumerate(pair_finder(globals())):
+            if len(bin) > input_width:
+                continue
+            srctok = tokenizer(src, padding = 'max_length', return_tensors = 'np')
+            if len(srctok['input_ids'][0]) > label_width:
+                continue
+            itok.write(bin.ljust(input_width, b'\0'))
+            iattn_buf[:len(bin)] = 1
+            iattn_buf[len(bin):] = 0
+            iattn.write(iattn_buf.data)
+            len1 = otok.write(srctok['input_ids'].astype(np.uint16).data)
+            len2 = oattn.write(srctok['attention_mask'].astype(np.uint8).data)
+            if len1 != label_width * 2 or len2 != label_width:
+                import pdb; pdb.set_trace()
+            if verbose and not (idx & 0xf):
+                print(str(idx), 'generated', end='\r', flush = True)
+    if verbose:
+        print()
+
+def read_files(pfx, tokenizerpfx, input_width, label_width):
+    import numpy as np
+    import mmap
+    with open('{pfx}bin.u8.{input_width}vec', 'rb') as itok, open('{pfx}bin.attnmask.u8.{input_width}vec', 'rb') as iattn, open(f'{pfx}{tokenizerpfx}src.u16.{label_width}vec', 'rb') as otok, open(f'{pfx}{tokenizerpfx}.src.attnmask.u8.{label_width}vec', 'rb') as oattn:
+        itok = np.frombuffer(mmap.mmap(itok.fileno(), 0, access = mmap.ACCESS_READ, offset = 0), np.uint8)
+        iattn = np.frombuffer(mmap.mmap(itok.fileno(), 0, access = mmap.ACCESS_READ, offset = 0), np.uint8)
+        otok = np.frombuffer(mmap.mmap(itok.fileno(), 0, access = mmap.ACCESS_READ, offset = 0), np.uint16)
+        oattn = np.frombuffer(mmap.mmap(itok.fileno(), 0, access = mmap.ACCESS_READ, offset = 0), np.uint16)
+    return dict(
+        input_ids = itok,
+        attention_mask = iattn,
+        decoder_input_ids = otok,
+        decoder_attention_mask = oattn
+    )
+
 if __name__ == '__main__':
-    for bin, src in pair_finder(globals()):
-        print(repr(bin), repr(src))
+    from transformers import T5Tokenizer
+    model = 't5-small'
+    tokenizer = T5Tokenizer.from_pretrained(model)
+    tokenizerpfx = model.replace('/','_') + '.'
+    write_files('', tokenizerpfx, 512, tokenizer, 512, globals())
+    result = read_files('', tokenizerpfx, 512, 512)
+    print(result['input_ids'][1].tobytes(), tokenizer.decode(result['decoder_input_ids'][1]))
