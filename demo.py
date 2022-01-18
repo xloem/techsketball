c7802b9aedc76350a76ca6fb1278edc218b9769c
xloem <0xloem@gmail.com>
2022-01-18 12:55:23 +0000

    demo.py, although little to show with it yet

--- demo.py
+++ demo.py
@@ -6,22 +6,34 @@ class Model:
     def __init__(self, model = 'baffo32/pyc2py_alpha'):
         self.tokenizer = transformers.T5Tokenizer.from_pretrained(model)
         self.model = transformers.FlaxT5ForConditionalGeneration.from_pretrained(model)
-        self.tokenizer.sp_model = spm.SentencePieceProcessor(tokenizer.vocab_file, **tokenizer.sp_kwargs)
+        self.tokenizer.sp_model = spm.SentencePieceProcessor(self.tokenizer.vocab_file, **self.tokenizer.sp_model_kwargs)
     def guess(self, bytes):
-        tokenized_input = self.tokenizer.encode(bytes, return_tensors='jax')
+        text = bytes.decode('iso-8859-1')
+        tokenized_input = self.tokenizer.encode(text, return_tensors='jax')
         output = self.model.generate(tokenized_input, pad_token_id=self.tokenizer.pad_token_id, do_sample=True)
-        return self.tokenizer.decode(output, skip_special_tokens=True)
+        return self.tokenizer.decode(output['sequences'][0], skip_special_tokens=True)
     @staticmethod
     def bytecode(func):
         import marshal
         return marshal.dumps(func.__code__)
+    @staticmethod
+    def set_default_backend(name = None):
+        import jax
+        if name is not None:
+            jax._src.lib.xla_bridge._default_backend = jax._src.lib.xla_bridge.backends()[name]
 
 if __name__ == '__main__':
-    def sum(a, b):
+    Model.set_default_backend()#'cpu')#'gpu')#'tpu')
+    model = Model()
+
+    def example_sum(a, b):
         return a + b
+
     import inspect
-    print(inspect.getsource(sum))
-    bytecode = Model.bytecode(sum)
+    print(inspect.getsource(example_sum))
+
+    bytecode = model.bytecode(example_sum)
     print(bytecode)
-    src = Model().guess(bytecode)
+
+    src = model.guess(bytecode)
     print(src)
